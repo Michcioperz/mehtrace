187d76f40e5afb5c3a9167d0bd37cf0e8ed39b8f
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2019-10-09 21:31:23 +0200

    clang-format

--- mehtrace.c
+++ mehtrace.c
@@ -1,55 +1,55 @@
+#include "syscallprint.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/user.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include "syscallprint.h"
 
 int mehtrace(pid_t child) {
   init_printfd();
-	fprintf(stderr, "%d\n", waitpid(child, NULL, 0));
-	if (ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_EXITKILL)) {
-		perror("failed to PTRACE_SETOPTIONS");
-		return EXIT_FAILURE;
-	}
-	for (;;) {
-		if (ptrace(PTRACE_SYSCALL, child, NULL, 0)) {
-			perror("failed to PTRACE_SYSCALL");
-			return EXIT_FAILURE;
-		}
-		waitpid(child, NULL, 0);
-		struct user_regs_struct regs;
-		if (ptrace(PTRACE_GETREGS, child, NULL, &regs)) {
-			perror("failed to PTRACE_GETREGS");
-			return EXIT_FAILURE;
-		}
+  fprintf(stderr, "%d\n", waitpid(child, NULL, 0));
+  if (ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_EXITKILL)) {
+    perror("failed to PTRACE_SETOPTIONS");
+    return EXIT_FAILURE;
+  }
+  for (;;) {
+    if (ptrace(PTRACE_SYSCALL, child, NULL, 0)) {
+      perror("failed to PTRACE_SYSCALL");
+      return EXIT_FAILURE;
+    }
+    waitpid(child, NULL, 0);
+    struct user_regs_struct regs;
+    if (ptrace(PTRACE_GETREGS, child, NULL, &regs)) {
+      perror("failed to PTRACE_GETREGS");
+      return EXIT_FAILURE;
+    }
     print_syscall(child, &regs);
-	}
+  }
 }
 
 int main(int argc, char *argv[]) {
-	(void)argc;
-	pid_t child = fork();
-	switch (child) {
-		case -1:
-			perror("failed to fork");
-			return EXIT_FAILURE;
-		case 0:
-			if (ptrace(PTRACE_TRACEME, 0, NULL, NULL)) {
-				perror("failed to PTRACE_TRACEME");
-				return EXIT_FAILURE;
-			}
-			if (raise(SIGSTOP)) {
-				perror("failed to raise SIGTRAP");
-				return EXIT_FAILURE;
-			}
-			execvp(argv[1], &argv[1]);
-			perror("failed to exec");
-			return EXIT_FAILURE;
-		default:
-			return mehtrace(child);
-	}
+  (void)argc;
+  pid_t child = fork();
+  switch (child) {
+  case -1:
+    perror("failed to fork");
+    return EXIT_FAILURE;
+  case 0:
+    if (ptrace(PTRACE_TRACEME, 0, NULL, NULL)) {
+      perror("failed to PTRACE_TRACEME");
+      return EXIT_FAILURE;
+    }
+    if (raise(SIGSTOP)) {
+      perror("failed to raise SIGTRAP");
+      return EXIT_FAILURE;
+    }
+    execvp(argv[1], &argv[1]);
+    perror("failed to exec");
+    return EXIT_FAILURE;
+  default:
+    return mehtrace(child);
+  }
 }
